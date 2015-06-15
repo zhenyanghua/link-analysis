4b01a2952ba416e046cb8332a31019b75b58b631
Zhenyang Hua <zhenyang.hua@woolpert.com>
2015-06-15 18:04:31 -0400

    implemented query builder selection logi

--- public/js/client.utils.js
+++ public/js/client.utils.js
@@ -1,17 +1,73 @@
 function loadModelstoUI() {
 	var filterCounter = 1;
-	$.each(MODELS, function(key, model) {
-		$('#sel-type').append('<option>' + model.type + '</option>');
+	var filterCounterCollection = {};
+	var $typeSelector = $('#sel-type');
+	$typeSelector.append('<option> Select One </option>');
+	$.each(_.pluck(MODELS,'type').sort(), function(key, type) {
+		$typeSelector.append('<option>' + type + '</option>');
 	})
+	$typeSelector.on('change', function(evt) {
+		changeQueryBuilderStatusHandler(evt, filterCounter, filterCounterCollection);
+	});
 	$('#add-filter').on('click', function(evt) { 
-		addFilterHandler(evt, filterCounter);
+		// record filterCounter Obj, add loaded as value to avoid property value changing.
+		filterCounterCollection[filterCounter] = { loaded: false };
+
+		addFilterHandler(evt, filterCounter, filterCounterCollection);
 		filterCounter++;
+	});
+	$('#run-query').on('click', runQueryHandler);
+}
+
+function changeQueryBuilderStatusHandler(evt, filterCounter, filterCounterCollection) {
+	loadRelationshipTypes(evt);
+	// Remove all filters when node type is changed.
+	removeFilterHandler(true, filterCounterCollection);
+}
+
+function loadRelationshipTypes(evt) {
+	var $relationshipSelector = $('#sel-relationships');
+	$relationshipSelector.empty();
+	$.each(MODELS, function(key, model) {
+		if (model.type != evt.target.value) return;
+		$.each(model.relationships.sort(), function(index, relationship) {
+			$relationshipSelector.append('<option>' + relationship + '</option>');
+		});
 	});
 }
 
-function addFilterHandler(evt, filterCounter) {
+function loadFilter(nodeTypeValue, filterCounterCollection) {
+	$.each(filterCounterCollection, function(filterCounter, val) {
+		var $propertiesSelector = $('#sel-property-' + filterCounter);
+		if (val.loaded == true) return;
+
+		$propertiesSelector.empty();
+		$.each(MODELS, function(_, model) {
+			if (model.type != nodeTypeValue) return;
+			$.each(model.properties, function(_, property) {
+				$propertiesSelector.append('<option>' + property + '</option>');
+			});
+		});
+
+		var $operatorSelector = $('#sel-operator-' + filterCounter);
+		$operatorSelector
+			.append('<option>=</option>')
+			.append('<option><></option>')
+			.append('<option><</option>')
+			.append('<option>></option>')
+			.append('<option><=</option>')
+			.append('<option>>=</option>')
+			.append('<option>IS NULL</option>')
+			.append('<option>IS NOT NULL</option>');
+
+		val.loaded = true;
+	});
+	
+}
+
+function addFilterHandler(evt, filterCounter, filterCounterCollection) {
 	evt.preventDefault();
-	console.log(filterCounter)
+	
 	$(evt.target).parent()
 		.before($('<div>').attr('id', 'filter-' + filterCounter).attr('class','panel panel-warning').css('margin-top', '5px')
 			.append($('<div>').attr('class','panel-heading')
@@ -22,17 +78,82 @@ function addFilterHandler(evt, filterCounter) {
 					.css('float', 'right')
 					.css('cursor', 'pointer')
 					.on('click', function() {
-						removeFilterHandler(filterCounter);
+						removeFilterHandler(false, filterCounterCollection, filterCounter);
 					}))))
 			.append($('<div>').attr('class','panel-body')
 				.append('<label for="sel-property-' + filterCounter + '">What type of Property?</label>')
 				.append('<select name="prop-' + filterCounter + '" class="form-control input-sm" id="sel-property-' + filterCounter + '"></select>')
-				.append('<label for="sel-operation-' + filterCounter + '">What type of operation?</label>')
-				.append('<select name="operation-' + filterCounter + '" class="form-control input-sm" id="sel-operation-' + filterCounter + '"></select>')
+				.append('<label for="sel-operator-' + filterCounter + '">What type of operator?</label>')
+				.append('<select name="operator-' + filterCounter + '" class="form-control input-sm" id="sel-operator-' + filterCounter + '"></select>')
 				.append('<label for="sel-property-value-' + filterCounter + '">What is the value?</label>')
-				.append('<input name="prop-value-' + filterCounter + '" type="text" class="form-control input-sm" id="sel-property-value-' + filterCounter + '">')))
+				.append('<input name="prop_value-' + filterCounter + '" type="text" class="form-control input-sm" id="sel-property-value-' + filterCounter + '">'))
+			// Animation css
+			.css({
+				height: 0,
+				opacity: 0.2
+			})
+			.animate({
+				height: "225px",
+				opacity: 1
+			}));
+	
+	// Triger Node Type Change event
+	var nodeTypeValue = $('#sel-type').val();
+	loadFilter(nodeTypeValue, filterCounterCollection);
 }
 
-function removeFilterHandler(filterCounter) {
-	$('#filter-' + filterCounter).remove();
+function removeFilterHandler(removeAll, filterCounterCollection, filterCounter) {
+	$.each(filterCounterCollection, function(key) {
+		if (removeAll == false) {
+			if (key != filterCounter) return;
+		}
+
+		$('#filter-' + key)
+			.animate({
+				height: 0,
+				opacity: 0.2
+			}, function() {
+				// Remove the container when animation is finished.
+				$(this).remove();
+			});
+
+		delete filterCounterCollection[filterCounter];
+	});	
+}
+
+function runQueryHandler() {
+	// Get form data
+	var form = $('form#query-builder')[0];
+	var data = getFormData(form);
+	// Create a newData object to hold reformmated data
+	var newData = {};
+	// Temporary filter array to hold each generated filter data
+	var filters = [];
+	// inherit "label" and "relationships" key/val pair, and push generated filter data to the temporary array.
+	$.each(data, function(key, val) {
+		if (key == "label" || key == "relationships") {
+			newData[key] = val;
+		} else {
+			var obj = {};
+			obj[key] = val;
+			filters.push(obj);
+		}
+	});
+	// Group filter by filterCounter number.
+	var newFilters = _.groupBy(filters, function(item) {
+		return Object.keys(item)[0].split('-')[1];
+	})
+	// Merge each key/val pair object.
+	var mergedFilters = {};
+	$.each(newFilters, function(index, array) {
+		var obj = {};
+		$.each(array, function(key, val) {
+			_.extend(obj,val);
+		})
+		mergedFilters[index] = obj;
+	})
+	// Only add filters to the newData Object if any exists.
+	if(_.size(mergedFilters) > 0) newData['filters'] = mergedFilters;
+	
+	console.log(newData)
 }
---
+++ public/lib/get-form-data.js
@@ -0,0 +1,187 @@
+/*!
+ * get-form-data 1.2.2 - https://github.com/insin/get-form-data
+ * MIT Licensed
+ */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.getFormData = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var NODE_LIST_CLASSES = {
+  '[object HTMLCollection]': true
+, '[object NodeList]': true
+, '[object RadioNodeList]': true
+}
+
+var IGNORED_INPUT_TYPES = {
+  'button': true
+, 'reset': true
+, 'submit': true
+, 'fieldset': true
+}
+
+var CHECKED_INPUT_TYPES = {
+  'checkbox': true
+, 'radio': true
+}
+
+var TRIM_RE = /^\s+|\s+$/g
+
+var slice = Array.prototype.slice
+var toString = Object.prototype.toString
+
+/**
+ * @param {HTMLFormElement} form
+ * @return {Object.<string,(string|Array.<string>)>} an object containing
+ *   submittable value(s) held in the form's .elements collection, with
+ *   properties named as per element names or ids.
+ */
+function getFormData(form, options) {
+  if (!form) {
+    throw new Error('A form is required by getFormData, was given form=' + form)
+  }
+
+  if (!options) {
+    options = {trim: false}
+  }
+
+  var data = {}
+  var elementName
+  var elementNames = []
+  var elementNameLookup = {}
+
+  // Get unique submittable element names for the form
+  for (var i = 0, l = form.elements.length; i < l; i++) {
+    var element = form.elements[i]
+    if (IGNORED_INPUT_TYPES[element.type] || element.disabled) {
+      continue
+    }
+    elementName = element.name || element.id
+    if (elementName && !elementNameLookup[elementName]) {
+      elementNames.push(elementName)
+      elementNameLookup[elementName] = true
+    }
+  }
+
+  // Extract element data name-by-name for consistent handling of special cases
+  // around elements which contain multiple inputs.
+  for (i = 0, l = elementNames.length; i < l; i++) {
+    elementName = elementNames[i]
+    var value = getNamedFormElementData(form, elementName, options)
+    if (value != null) {
+      data[elementName] = value
+    }
+  }
+
+  return data
+}
+
+/**
+ * @param {HTMLFormElement} form
+ * @param {string} elementName
+ * @return {(string|Array.<string>)} submittable value(s) in the form for a
+ *   named element from its .elements collection, or null if there was no
+ *   element with that name or the element had no submittable value(s).
+ */
+function getNamedFormElementData(form, elementName, options) {
+  if (!form) {
+    throw new Error('A form is required by getNamedFormElementData, was given form=' + form)
+  }
+  if (!elementName && toString.call(elementName) !== '[object String]') {
+    throw new Error('A form element name is required by getNamedFormElementData, was given elementName=' + elementName)
+  }
+
+  var element = form.elements[elementName]
+  if (!element || element.disabled) {
+    return null
+  }
+
+  var trim = !!(options && options.trim)
+
+  if (!NODE_LIST_CLASSES[toString.call(element)]) {
+    return getFormElementValue(element, trim)
+  }
+
+  // Deal with multiple form controls which have the same name
+  var data = []
+  var allRadios = true
+  for (var i = 0, l = element.length; i < l; i++) {
+    if (element[i].disabled) {
+      continue
+    }
+    if (allRadios && element[i].type !== 'radio') {
+      allRadios = false
+    }
+    var value = getFormElementValue(element[i], trim)
+    if (value != null) {
+      data = data.concat(value)
+    }
+  }
+
+  // Special case for an element with multiple same-named inputs which were all
+  // radio buttons: if there was a selected value, only return the value.
+  if (allRadios && data.length === 1) {
+    return data[0]
+  }
+
+  return (data.length > 0 ? data : null)
+}
+
+/**
+ * @param {HTMLElement} element a form element.
+ * @param {booleam} trim should values for text entry inputs be trimmed?
+ * @return {(string|Array.<string>|File|Array.<File>)} the element's submittable
+ *   value(s), or null if it had none.
+ */
+function getFormElementValue(element, trim) {
+  var value = null
+
+  if (element.type === 'select-one') {
+    if (element.options.length) {
+      value = element.options[element.selectedIndex].value
+    }
+    return value
+  }
+
+  if (element.type === 'select-multiple') {
+    value = []
+    for (var i = 0, l = element.options.length; i < l; i++) {
+      if (element.options[i].selected) {
+        value.push(element.options[i].value)
+      }
+    }
+    if (value.length === 0) {
+      value = null
+    }
+    return value
+  }
+
+  // If a file input doesn't have a files attribute, fall through to using its
+  // value attribute.
+  if (element.type === 'file' && 'files' in element) {
+    if (element.multiple) {
+      value = slice.call(element.files)
+      if (value.length === 0) {
+        value = null
+      }
+    }
+    else {
+      // Should be null if not present, according to the spec
+      value = element.files[0]
+    }
+    return value
+  }
+
+  if (!CHECKED_INPUT_TYPES[element.type]) {
+    value = (trim ? element.value.replace(TRIM_RE, '') : element.value)
+  }
+  else if (element.checked) {
+    value = element.value
+  }
+
+  return value
+}
+
+getFormData.getNamedFormElementData = getNamedFormElementData
+
+module.exports = getFormData
+},{}]},{},[1])(1)
+});
